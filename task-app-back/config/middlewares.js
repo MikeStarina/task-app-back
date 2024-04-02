12ab39f72085c1907c20e973e479f64445741a1a
Mike Starina <x@pnhd.ru>
2024-04-02 16:05:48 +0300

    initial

--- task-app-back/config/middlewares.js
+++ task-app-back/config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     config: {
       enabled: true,
       header: '*',
-      origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:1337']
+      origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:1337', 'https://tasks-app-xi-two.vercel.app']
     }
   },
   'strapi::poweredBy',
--- task-app-back/types/generated/contentTypes.d.ts
+++ task-app-back/types/generated/contentTypes.d.ts
@@ -818,6 +818,38 @@ export interface ApiModelTypeModelType extends Schema.CollectionType {
   };
 }
 
+export interface ApiOrderOrder extends Schema.CollectionType {
+  collectionName: 'orders';
+  info: {
+    singularName: 'order';
+    pluralName: 'orders';
+    displayName: 'Orders';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  attributes: {
+    number: Attribute.String & Attribute.Required;
+    order: Attribute.JSON & Attribute.Required;
+    user: Attribute.Integer & Attribute.Required;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiPassportPassport extends Schema.CollectionType {
   collectionName: 'passports';
   info: {
@@ -931,6 +963,7 @@ declare module '@strapi/types' {
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
       'api::model-type.model-type': ApiModelTypeModelType;
+      'api::order.order': ApiOrderOrder;
       'api::passport.passport': ApiPassportPassport;
       'api::size.size': ApiSizeSize;
       'api::supplier.supplier': ApiSupplierSupplier;
